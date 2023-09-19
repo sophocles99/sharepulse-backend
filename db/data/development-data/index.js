61317bd80c317e80207ad1e4f8c8e7fedcb2f221
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-09-19 20:48:41 +0100

    seed portfolios table

--- db/data/development-data/index.js
+++ db/data/development-data/index.js
@@ -1,4 +1,5 @@
 import profileData from "./profiles.js";
 import userData from "./users.js";
+import portfolioData from "./portfolios.js";
 
-export default { profileData, userData };
+export default { profileData, userData, portfolioData };
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -2,32 +2,45 @@ import bcrypt from "bcrypt";
 import format from "pg-format";
 import db from "../connection.js";
 
-const seed = ({ userData, profileData }) => {
+const seed = ({ userData, profileData, portfolioData }) => {
   return db
-    .query(`DROP TABLE IF EXISTS profiles;`)
+    .query(`DROP TABLE IF EXISTS portfolios;`)
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS profiles;`);
+    })
     .then(() => {
       return db.query(`DROP TABLE IF EXISTS users;`);
     })
     .then(() => {
-      return db.query(`CREATE TABLE users (
-        user_id SERIAL PRIMARY KEY,
-        email VARCHAR(254) NOT NULL UNIQUE,
-        password VARCHAR NOT NULL,
-        last_login TIMESTAMP DEFAULT NULL
-      )`);
+      return db.query(`
+        CREATE TABLE users (
+          user_id SERIAL PRIMARY KEY,
+          email VARCHAR(254) NOT NULL UNIQUE,
+          password VARCHAR NOT NULL,
+          last_login TIMESTAMP DEFAULT NULL
+        )`);
     })
     .then(() => {
-      return db.query(`CREATE TABLE profiles (
-        user_id INT PRIMARY KEY REFERENCES users(user_id) ON DELETE CASCADE,
-        first_name VARCHAR(255) NOT NULL,
-        last_name VARCHAR(255) NOT NULL,
-        birth_date DATE NOT NULL,
-        address_1 VARCHAR(255) NOT NULL,
-        address_2 VARCHAR(255) NOT NULL,
-        address_3 VARCHAR(255),
-        address_4 VARCHAR(255),
-        postcode VARCHAR(8) NOT NULL
-    );`);
+      return db.query(`
+        CREATE TABLE profiles (
+          user_id INT PRIMARY KEY REFERENCES users ON DELETE CASCADE,
+          first_name VARCHAR(255) NOT NULL,
+          last_name VARCHAR(255) NOT NULL,
+          birth_date DATE NOT NULL,
+          address_1 VARCHAR(255) NOT NULL,
+          address_2 VARCHAR(255) NOT NULL,
+          address_3 VARCHAR(255),
+          address_4 VARCHAR(255),
+          postcode VARCHAR(8) NOT NULL
+        );`);
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE portfolios (
+          portfolio_id SERIAL PRIMARY KEY,
+          user_id INT REFERENCES users ON DELETE CASCADE,
+          name VARCHAR(255) NOT NULL
+        );`);
     })
     .then(() => {
       const userHashPromises = userData.map(async ({ email, password }) => ({
@@ -38,10 +51,11 @@ const seed = ({ userData, profileData }) => {
     })
     .then((usersWithHashedPasswords) => {
       const insertUsersQueryStr = format(
-        `INSERT INTO users (
-        email,
-        password
-      ) VALUES %L;`,
+        `
+        INSERT INTO users (
+          email,
+          password
+        ) VALUES %L;`,
         usersWithHashedPasswords.map(({ email, hashedPassword }) => [
           email,
           hashedPassword,
@@ -51,16 +65,17 @@ const seed = ({ userData, profileData }) => {
     })
     .then(() => {
       const insertProfilesQueryStr = format(
-        `INSERT INTO profiles (
-        user_id,
-        first_name,
-        last_name,
-        birth_date,
-        address_1,
-        address_2,
-        address_3,
-        address_4,
-        postcode
+        `
+        INSERT INTO profiles (
+          user_id,
+          first_name,
+          last_name,
+          birth_date,
+          address_1,
+          address_2,
+          address_3,
+          address_4,
+          postcode
         ) VALUES %L;`,
         profileData.map(
           ({
@@ -87,6 +102,17 @@ const seed = ({ userData, profileData }) => {
         )
       );
       return db.query(insertProfilesQueryStr);
+    })
+    .then(() => {
+      const insertPortfoliosQueryStr = format(
+        `
+        INSERT INTO portfolios (
+          user_id,
+          name
+        ) VALUES %L;`,
+        portfolioData.map(({ user_id, name }) => [user_id, name])
+      );
+      return db.query(insertPortfoliosQueryStr);
     });
 };
 
