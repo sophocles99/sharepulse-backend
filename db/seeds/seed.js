f5a73a7197a5857de329588cd29a08203ff7e0ab
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-09-22 11:55:01 +0100

    move database definitions into separate file

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,5 +1,14 @@
 import bcrypt from "bcrypt";
 import db from "../connection.js";
+import {
+  createUsersStr,
+  createProfilesStr,
+  createPortfoliosStr,
+  createCashHoldingsStr,
+  createSharesStr,
+  createPortfolioHoldingsStr,
+  createTransactionsStr,
+} from "./db-definitions.js";
 import insertDataIntoDb from "./utils/insert-data-into-db.js";
 
 const seed = ({
@@ -32,94 +41,25 @@ const seed = ({
       return db.query(`DROP TABLE IF EXISTS users;`);
     })
     .then(() => {
-      return db.query(`
-        CREATE TABLE users (
-          user_id SERIAL PRIMARY KEY,
-          email VARCHAR(254) NOT NULL UNIQUE,
-          password VARCHAR NOT NULL,
-          last_login TIMESTAMP DEFAULT NULL
-        )`);
+      return db.query(createUsersStr);
     })
     .then(() => {
-      return db.query(`
-        CREATE TABLE profiles (
-          user_id INT PRIMARY KEY REFERENCES users ON DELETE RESTRICT,
-          first_name VARCHAR(255) NOT NULL,
-          last_name VARCHAR(255) NOT NULL,
-          birth_date DATE NOT NULL,
-          address_1 VARCHAR(255) NOT NULL,
-          address_2 VARCHAR(255) NOT NULL,
-          address_3 VARCHAR(255),
-          address_4 VARCHAR(255),
-          postcode VARCHAR(8) NOT NULL
-        );`);
+      return db.query(createProfilesStr);
     })
     .then(() => {
-      return db.query(`
-        CREATE TABLE portfolios (
-          portfolio_id SERIAL PRIMARY KEY,
-          user_id INT REFERENCES users ON DELETE CASCADE,
-          name VARCHAR(255) NOT NULL
-        );`);
+      return db.query(createPortfoliosStr);
     })
     .then(() => {
-      return db.query(`
-        CREATE TABLE cash_holdings (
-          user_id INT PRIMARY KEY REFERENCES users ON DELETE RESTRICT,
-          amount NUMERIC(9, 2)
-        );`);
+      return db.query(createCashHoldingsStr);
     })
     .then(() => {
-      return db.query(`
-        CREATE TABLE shares (
-          share_id SERIAL PRIMARY KEY,
-          symbol VARCHAR(10) UNIQUE,
-          company_name VARCHAR(255),
-          description TEXT,
-          exchange VARCHAR(10),
-          currency VARCHAR(3),
-          country VARCHAR(50),
-          sector VARCHAR(50),
-          industry VARCHAR(50)
-        )`);
+      return db.query(createSharesStr);
     })
     .then(() => {
-      return db.query(`
-        CREATE TABLE portfolio_holdings (
-          portfolio_holdings_id SERIAL PRIMARY KEY,
-          portfolio_id INT REFERENCES portfolios ON DELETE RESTRICT,
-          share_id INT REFERENCES shares ON DELETE RESTRICT,
-          quantity NUMERIC(11, 4)
-        )`);
+      return db.query(createPortfolioHoldingsStr);
     })
     .then(() => {
-      return db.query(`
-        CREATE TABLE transactions (
-          transaction_id SERIAL PRIMARY KEY,
-          user_id INT NOT NULL REFERENCES users ON DELETE RESTRICT,
-          type CHAR(1) NOT NULL CHECK (type IN ('D', 'W', 'B', 'S')),
-          date_time TIMESTAMP NOT NULL,
-          share_id INT REFERENCES shares ON DELETE RESTRICT,
-          portfolio_id INT REFERENCES portfolios ON DELETE RESTRICT,
-          quantity NUMERIC(11, 4),
-          unit_price NUMERIC(11, 4),
-          total_amount NUMERIC(11, 4) NOT NULL,
-          CONSTRAINT "share_id: null for cash transaction, required for trade"
-            CHECK (((type = 'D' OR type = 'W') AND share_id IS NULL) OR
-                  ((type = 'B' OR type = 'S') AND share_id IS NOT NULL)),
-          CONSTRAINT "portfolio_id: null for cash transaction, required for trade"
-            CHECK (((type = 'D' OR type = 'W') AND portfolio_id IS NULL) OR
-                  ((type = 'B' OR type = 'S') AND portfolio_id IS NOT NULL)),
-          CONSTRAINT "quantity: null for cash transaction, required for trade"
-            CHECK (((type = 'D' OR type = 'W') AND quantity IS NULL) OR
-                  ((type = 'B' OR type = 'S') AND quantity IS NOT NULL)),
-          CONSTRAINT "unit_price: null for cash transaction, required for trade"
-            CHECK (((type = 'D' OR type = 'W') AND unit_price IS NULL) OR
-                  ((type = 'B' OR type = 'S') AND unit_price IS NOT NULL)),
-          CONSTRAINT "total_amount: must equal quantity * unit_price"
-            CHECK (((type = 'B' OR type = 'S') AND total_amount = ROUND(quantity * unit_price, 4)) OR
-                  (type = 'D' OR type = 'W'))
-        )`);
+      return db.query(createTransactionsStr);
     })
     .then(() => {
       const userHashPromises = userData.map(async ({ email, password }) => ({
