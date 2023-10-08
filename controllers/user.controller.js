0ac09bf617e46901bd7cf62c3ffbb62f05adb0d9
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-10-08 18:06:43 +0100

    handle user already exists error

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,16 +3,20 @@ import validateUser from "../validation/validate-user.js";
 
 const registerUser = (req, res, next) => {
   const { user } = req.body;
-  if (!user) return res.status(400).send({ msg: "user object required" });
+  if (!user) return res.status(400).send({ msg: "User required" });
 
   const { error } = validateUser(user);
   if (error) return res.status(400).send({ msg: error.details[0].message });
 
   insertUser(user)
     .then((user_id) => {
-      res.status(201).send({ user_id });
+      res.status(201).send({ msg: "User registered", user_id });
     })
-    .catch(next);
+    .catch((err) => {
+      if (err.code === "23505")
+        return res.status(409).send({ msg: "User already exists" });
+      next(err);
+    });
 };
 
 const loginUser = (req, res, next) => {
